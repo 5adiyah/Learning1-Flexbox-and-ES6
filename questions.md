0ff2eb6d1b786a654a8c5cfd428bd5599356d9e6
5adiyah <sadiali@pdx.edu>
2017-02-16 22:43:05 -0800

    Add scrolling feature

---
+++ questions.md
@@ -0,0 +1,26 @@
+**Questions I've Looked up**
+
+- How to add trigger an event on hover
+  `foo.addEventListener('mouseenter', function(e){}`
+
+- What's the difference between mouseenter and mouseover?
+  -- *Mouseover* Will fire upon entering an element and whenever any mouse movements occur within the element.
+  -- *Mouseenter* Will fire upon entering an element.
+  -- Each time your mouse enters or leaves a child element, mouseover is triggered, but not mouseenter.
+
+
+All blue
+  Blue: 0, 703              703 Difference
+  Yellow: 703, 1406         
+
+2/3 blue
+  Blue: -178, 525
+  Yellow: 525, 1228
+
+1/3 blue
+  Blue: -499, 204
+  Yellow: 204, 907
+
+All Yellow
+  Blue: -703, 0
+  Yellow: 0, 703
--- src/app.js
+++ src/app.js
@@ -1,2 +1,44 @@
-let scrollDiv = document.querySelector('.scroll');
-console.log(scrollDiv);
+const scrollDiv = document.querySelector('.scroll');
+const scrollImg = document.querySelector('.scrollImg');
+
+scrollDiv.addEventListener('mouseenter', function(e){
+  scrollDiv.classList.add('scrollHover');
+  scrollImg.src = 'src/images/nextHover.png';
+});
+
+scrollDiv.addEventListener('mouseleave', function(e){
+  scrollDiv.classList.remove('scrollHover');
+  scrollImg.src = 'src/images/next.png';
+});
+
+// var slide1 = document.querySelector('.slide1');
+// var slide1Top = slide1.getBoundingClientRect().top;
+// var slide1Bottom = slide1.getBoundingClientRect().bottom;
+// var slide1Visible = (slide1Top >=0) && (slide1Bottom <= window.innerHeight);
+//
+// var slide2 = document.querySelector('.slide2');
+// var slide2Top = slide2.getBoundingClientRect().top;
+// var slide2Bottom = slide2.getBoundingClientRect().bottom;
+// var slide2Visible = (slide2Top >=0) && (slide2Bottom <= window.innerHeight);
+//
+// var slide3 = document.querySelector('.slide3');
+// var slide3Top = slide3.getBoundingClientRect().top;
+// var slide3Bottom = slide3.getBoundingClientRect().bottom;
+// var slide3Visible = (slide3Top >=0) && (slide3Bottom <= window.innerHeight);
+
+scrollDiv.addEventListener('click', function(e){
+  window.scrollBy(0, window.innerHeight);
+});
+
+
+
+function isScrolledIntoView(el) {
+    var elemTop = el.getBoundingClientRect().top;
+    var elemBottom = el.getBoundingClientRect().bottom;
+
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
+
+
+//slide2.scrollIntoView()
